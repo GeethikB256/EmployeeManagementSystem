baf28ce4c56eb11be101da8a1c11d4f59d7da3a1
Geethika05 <b.geethika@capgemini.com>
2026-07-27 13:10:34 +0530

    Fix flyway initial migration

--- src/main/resources/db/migration/V1__create_employees_table.sql
+++ src/main/resources/db/migration/V1__create_employees_table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE employees (
                            id BIGSERIAL PRIMARY KEY,
-                           first_name VARCHAR(35) NOT NULL,
-                           last_name VARCHAR(20) NOT NULL,
+                           firstname VARCHAR(35) NOT NULL,
+                           lastname VARCHAR(20) NOT NULL,
                            email VARCHAR(50) NOT NULL UNIQUE,
                            department VARCHAR(45) NOT NULL,
-                           job_title VARCHAR(30) NOT NULL,
+                           jobTitle VARCHAR(30) NOT NULL,
                            salary DOUBLE PRECISION NOT NULL
 );
